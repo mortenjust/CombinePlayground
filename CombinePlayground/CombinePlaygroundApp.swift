45ed3eff3d20b025388a6a80f23a21ac764f7785
Morten Just <morten.just@genstart.dk>
2022-04-07 11:30:58 +0200

    Initial Commit

---
+++ CombinePlayground/CombinePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePlaygroundApp.swift
+//  CombinePlayground
+//
+//  Created by Morten Just on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CombinePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
